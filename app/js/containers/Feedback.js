110af95f59c4e8c9f467877a376f85a88b42c9f9
keen <ehrudxo@hotmail.com>
2015-12-16 23:22:48 +0900

    remove handler

--- app/js/containers/Feedback.js
+++ app/js/containers/Feedback.js
@@ -14,24 +14,6 @@ class Feedback extends React.Component{
       title : '저와 함께한 pair 코딩은 어땠나요?',
       text : '지난 일주일 저와 함께 해 주신 팀원 여러분 일단 감사 드립니다.\n 그동안 저와 함께 했던 pairing 에 대해 허심탄회하게 적어주시면 제가 앞으로 발전하는데 좋은 밑거름이 되도록 노력하겠습니다.'
     }
-    this.formSubmit = this.formSubmit.bind(this);
-  }
-  formSubmit( feedbackText ){
-    let urlStr = "http://localhost:3500/feedback";
-    $.ajax( {
-      type : "POST",
-      url: urlStr,
-      dataType: "json",
-      cache: false,
-      data : {
-        "text"  : feedbackText,
-        "token" : this.token
-      },
-      success: (data)=> {
-        this.setState(Object.assign({isSubmitted : true},data));
-      }
-    });
-
   }
   render(){
     const {dispatch} = this.props;
